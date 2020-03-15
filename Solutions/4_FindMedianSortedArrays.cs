c31ee6a2792b2cda539999f2e790df6a329b8cb8
ZhangJiale <Zhang.Jiale@britishcouncil.org.cn>
2020-03-15 19:36:13 +0800

    Commit 4. FindMedianSortedArrays.

---
+++ Solutions/4_FindMedianSortedArrays.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public partial class Solution
+    {
+        /*
+4. 寻找两个有序数组的中位数
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+示例 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+示例 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+        */
+
+        /// <summary>
+        /// 4.FindMedianSortedArrays
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns></returns>
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int sum = nums1.Length + nums2.Length;
+
+            if (sum % 2 == 0)
+            {
+                return ((double)findKth(nums1, 0, nums2, 0, sum / 2) + (double)findKth(nums1, 0, nums2, 0, sum / 2 + 1)) / 2;
+            }
+
+            return findKth(nums1, 0, nums2, 0, (sum + 1) / 2);
+        }
+
+        public static int findKth(int[] nums1, int left1, int[] nums2, int left2, int k)
+        {
+            if (left1 >= nums1.Length) return nums2[left2 + k - 1];
+            if (left2 >= nums2.Length) return nums1[left1 + k - 1];
+            if (k == 1) return Math.Min(nums1[left1], nums2[left2]);
+
+            int mid1 = left1 + k / 2 - 1 < nums1.Length ? nums1[left1 + k / 2 - 1] : int.MaxValue;
+            int mid2 = left2 + k / 2 - 1 < nums2.Length ? nums2[left2 + k / 2 - 1] : int.MaxValue;
+
+            if (mid1 <= mid2)
+            {
+                return findKth(nums1, left1 + k / 2, nums2, left2, k - k / 2);
+            }
+            return findKth(nums1, left1, nums2, left2 + k / 2, k - k / 2);
+        }
+    }
+}
---
+++ Tests/FindMedianSortedArraysTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Solutions;
+
+namespace Tests
+{
+    public class FindMedianSortedArraysTests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 3, 5, 7, 9, 11, 12, 13, 14 }, new int[] { 2, 4, 6, 8, 10 }, 7.5)]
+        [TestCase(new int[] { 1, 3 }, new int[] { 2 }, 2)]
+        [TestCase(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
+        public void should_find_median_in_two_sorted_arrays(int[] nums1, int[] nums2, double expected)
+        {
+            // Act
+            var actual = Solution.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
